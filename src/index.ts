449d23d1d512301421bcee7d375158315fbb1ecb
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-01-14 20:22:00 +0530

    ⭕ Express Server Running

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 const main = async () => {
   // 1. Connect to the database
   // 2. Run migrations
   // 3. Run sql
 
   const orm = await MikroORM.init(microConfig); // Basically we got the type that init expects for its first parameter
-
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" }); // This creates a post object
-  await orm.em.persistAndFlush(post); // This pushes the post object inside the table.
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log("server started on localhost: 4000"); // We listen to the app on port 4000
+  });
 };
 
 main().catch((err) => {
