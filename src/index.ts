99d118b8f8f04bc149013c8902555038aa81ca7b
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-01-15 16:08:22 +0530

    🏮 graphql playground working

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
   // 1. Connect to the database
   // 2. Run migrations
@@ -10,10 +13,17 @@ const main = async () => {
 
   const orm = await MikroORM.init(microConfig); // Basically we got the type that init expects for its first parameter
   await orm.getMigrator().up();
+
   const app = express();
-  app.get("/", (_, res) => {
-    res.send("hello");
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
+
+  apolloServer.applyMiddleware({ app });
   app.listen(4000, () => {
     console.log("server started on localhost: 4000"); // We listen to the app on port 4000
   });
