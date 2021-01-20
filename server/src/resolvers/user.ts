74ddefcc0c0573f031db97f966fec2202a8edc94
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-01-20 15:35:09 +0530

    🚇 FrontEnd Added

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -48,7 +48,7 @@ export class UserResolver {
     if (!req.session.userId) {
       return null;
     }
-    const user = await em.findOne(User, { id: req.session!.userId });
+    const user = await em.findOne(User, { id: req.session.userId });
     return user;
   }
 
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,8 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import { Request, Response } from "express";
-import session from "express-session";
+// import session from "express-session";
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session?: session.Session & Partial<session.SessionData> };
+  req: Request; // & { session?: session.Session & Partial<session.SessionData> };
   res: Response;
 };
