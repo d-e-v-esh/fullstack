d9a51e091ba10aa54984dbf607f631bacd90c5bb
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-01-22 14:01:17 +0530

    📜 Updated Web sconfig

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -11,30 +11,22 @@ import {
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  const REG_MUT = `mutation Register($username: String!, $password: String!) {
-  register(options: { username: $username, password: $password }) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}`;
-  const [{}, register] = useMutation(REG_MUT);
+  const [{}, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-          return register(values); // The keys line up exactly with the values so we don't need to specify each one => username => username && password => password
+        onSubmit={async (values, {setErrors}) => {
+          // The keys line up exactly with the values so we don't need to specify each one => username => username && password => password
+          const response = await register(values);
+
+          if(response.data?.register.errors)
         }}>
+
         {({ values, handleChange, isSubmitting }) => (
           <Form>
             <InputField
