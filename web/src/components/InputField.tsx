8abc9c2ba3a58d91f2397f96406fcfae7022c9b4
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-02-02 22:35:34 +0530

    📮 Working On CreatePost

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
@@ -12,18 +13,24 @@ import React, { InputHTMLAttributes } from "react";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string; // making name required
   label: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id="name" />
+      <InputOrTextarea {...field} {...props} id="name" />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
